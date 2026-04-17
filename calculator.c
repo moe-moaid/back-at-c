86102b3759efaf32db4e282f1bf8d99d33244db7
mohammad moaid <mohammadmoaid@gmail.com>
2026-04-17 17:12:25 +0500

    no idea what is happening

--- calculator.c
+++ calculator.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 #include <math.h>
+#include <regex.h>
+#include <ctype.h>
 
-int sanitize_input(int* arr, int arr_size) {
-  int sum = 0;
-  for(int x = 0; x < arr_size; x++) {
-    sum += *(arr + x); // finally found it, this is how to dereference pointers outside printf statement
+int is_operator(char c) {
+  return c == '+' || c == '-' || c == '/' || c == '*';
+}
+
+char sanitize_input(char* arr, int arr_size) {
+  int m = 0, n = 0;
+  while (arr[m] != "\0"){
+    if(isdigit(arr[m]) || is_operator(arr[m])) {
+      arr[n++] = arr[m];
+      printf("$c\n", arr[m]);
+    }
+    m++;
   }
-  return sum;
+  return 0;
 }
 
 void calculator() {
-  int some_arr[] = {1, 2, 3, 4, 5};
-  int arr_size = sizeof(some_arr)/sizeof(some_arr[0]); 
-  printf("%d", sanitize_input(some_arr, arr_size));
+  char input[128];
+  int arr_size = sizeof(input)/sizeof(input[0]); 
+  fgets(input, sizeof(input), stdin);
+  sanitize_input(input, arr_size);
 }
 
 int main() {
